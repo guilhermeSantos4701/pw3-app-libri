0b1f9b1988847c8fb32e75961ccf0826399a3e82
guilhermeSantos4701 <guilhermesantos4701@gmail.com>
2024-08-01 22:21:45 -0300

    Limpeza dos arquivos css e criação do primeiro componente

--- projeto1/src/App.jsx
+++ projeto1/src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import CardBooks from './components/CardBooks'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    // fragments
+    <>  
+      <CardBooks
+        titulo= 'BTK: Máscara da Maldade'
+        autor= 'Dennis Lynn Rader'
+      /> 
+
+      <CardBooks
+        titulo= 'A paciente silenciosa'
+        autor= 'Alex Michaelides'
+      />
+
+      <CardBooks
+        titulo= 'O mítico homem-mês'
+        autor= 'Fred Brooks'
+      /> 
     </>
   )
 }
